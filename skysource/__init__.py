83fcbca247b22ba5b26a08568c7b322ee046664e
da.sanfratello <da.sanfratello@gmail.com>
2021-04-06 19:51:57 +0200

    Created `SkySource` class that is initialized with a MC random generator for mass and then its luminosity and magnitude are calculated accordingly.

---
+++ skysource/__init__.py
@@ -0,0 +1,1 @@
+from skysource import SkySource
---
+++ skysource/skysource.py
@@ -0,0 +1,45 @@
+import numpy as np
+import random as rd
+
+from fieldsim.excep import WrongCoordsFormatError
+from fieldsim.excep import WrongCoordsLengthError
+
+
+class SkySource:
+    def __init__(self, coords):
+        try:
+            iter(coords)
+        except TypeError:
+            raise WrongCoordsFormatError
+        else:
+            if len(coords) != 2:
+                raise WrongCoordsLengthError
+
+        self.coords = coords
+
+        self.mass = None
+        self.luminosity = None
+        self.magnitude = None
+
+    def initialize(self, e_imf=2.4, e_lm=3, cst_lm=1):
+        self.mass = self.random_cimf(rd.random, e=e_imf)
+        self.luminosity = self.lm_relation(self.mass, e=e_lm, cst=cst_lm)
+        self.magnitude = self.l2mag(self.luminosity)
+
+    @staticmethod
+    def random_cimf(cimf, e=2.4):
+        if e <= 1:
+            raise ValueError("IMF exponent cannot be smaller or equal to 1.")
+
+        return (cimf + 1) ** (1 / (1 - e))
+
+    @staticmethod
+    def lm_relation(mass, e=3, cst=1):
+        if e <= 0:
+            raise ValueError("Luminosity increases with mass, usually.")
+
+        return cst * mass ** (-e)
+
+    @staticmethod
+    def l2mag(luminosity):
+        return -2.5 * np.log10(luminosity)

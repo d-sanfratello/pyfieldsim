052e4fa855751e689c22fe8c4ec838a18970cee1
da.sanfratello <da.sanfratello@gmail.com>
2021-04-14 20:28:02 +0200

    Created first test for checks.

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
+import matplotlib.pyplot as plt
+
 from fieldsim.field import Field
 from fieldsim.observation import Observation
+
+if __name__ == "__main__":
+    fld = Field((200, 200))
+    fld.initialize_field(density=0., datatype='mass')
+
+    fld.show_field(field='true')
+
+    obs = Observation(fld)
+
+    print(obs.datatype, obs.status)
+
+    stars, coords = obs.count_single_stars()
+
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.grid()
+    hist, edges, patch = ax.hist(stars, bins='auto', log=True)
+
+    plt.show()

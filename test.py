6c7f969e46b18ca0f2c4d8620fe7c99ae803a3af
da.sanfratello <da.sanfratello@gmail.com>
2021-04-15 23:29:48 +0200

    Added checks for photon noise in sources and gain map.

--- test.py
+++ test.py
@@ -6,23 +6,45 @@ from fieldsim.observation import Observation
 
 if __name__ == "__main__":
     fld = Field((200, 200))
-    fld.initialize_field(density=0.05, datatype='mass')
+    fld.initialize_field(density=0.02, cst_lm=100,
+                         datatype='luminosity')
 
     fld.show_field(field='true')
 
     obs = Observation(fld)
 
-    print(obs.datatype, obs.status)
+    print("datatype  : {}".format(obs.datatype))
+    print("obs_status: {}".format(obs.status))
 
     stars, coords = obs.count_single_stars()
 
     hist, edges = np.histogram(stars, bins='sqrt')
-    bin_edges = np.logspace(0, np.log10(edges[-1]), len(edges))
+    bin_edges = np.logspace(np.log10(edges[0]), np.log10(edges[-1]), len(edges))
 
     fig = plt.figure()
     ax = fig.gca()
     ax.grid()
     hist, edges, patch = ax.hist(stars, bins=bin_edges, log=True)
     ax.set_xscale('log')
+
+    # Adding photon noise to sources.
+    fld.add_photon_noise()
+    fld.show_field(field='ph_noise')
+    obs.update_image()
+
+    stars, coords = obs.count_single_stars()
+
+    hist, edges = np.histogram(stars, bins='sqrt')
+    bin_edges = np.logspace(np.log10(edges[0]), np.log10(edges[-1]), len(edges))
+
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.grid()
+    hist, edges, patch = ax.hist(stars, bins=bin_edges, log=True)
+    ax.set_xscale('log')
+
+    # Creating gain map.
+    fld.create_gain_map()
+    fld.show_field(field='gain_map')
 
     plt.show()

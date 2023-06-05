249d24f335897d3211ac2f60672b193c161dff78
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-06-05 11:02:44 +0200

    Implemented 'save_metadata' function. Reading metadata from sources file.

--- pyfieldsim/core/fieldtypes/noise.py
+++ pyfieldsim/core/fieldtypes/noise.py
@@ -3,6 +3,7 @@ import numpy as np
 from pathlib import Path
 
 from pyfieldsim.core.fieldtypes.field import Field
+from pyfieldsim.utils.metadata import read_metadata, save_metadata
 
 
 def ph_noise(sources_file, delta_time):
@@ -22,13 +23,21 @@ def ph_noise(sources_file, delta_time):
         w_ph_noise < 0, 0, w_ph_noise
     )
 
+    metadata = {
+        "delta_time": delta_time
+    }
+
+    save_metadata(
+        metadata=metadata,
+        filename='P' + sources_file.name[1:]
+    )
+
     return Field(
         w_ph_noise,
         seed=sources_field.metadata['_seed'],
         sources_file=str(sources_file),
         ph_noise_file=None,
         bkgnd_file=None,
-        psf_file=None,
         gain_map_file=None,
         dk_c_file=None
     )
@@ -41,6 +50,8 @@ def background(sources_file,
     ph_noise_field_filename = 'P' + sources_file.name[1:]
     ph_noise_field_file = Field.from_field(ph_noise_field_filename)
 
+    sim_meta = read_metadata(sources_file)
+
     bgst_star = np.max(ph_noise_field_file.field)
 
     mean_bgnd = bgst_star / snr
@@ -49,13 +60,24 @@ def background(sources_file,
     elif sigma is None:
         raise ValueError("Must pass a sigma or relative sigma value.")
 
-    rng = np.random.default_rng(seed=ph_noise_field_file.metadata['_seed'])
+    metadata = {
+        "mean": mean_bgnd,
+        "sigma": sigma,
+        "rel_var": rel_var
+    }
+
+    save_metadata(
+        metadata=metadata,
+        filename='B' + sources_file.name[1:]
+    )
+
+    rng = np.random.default_rng(seed=sim_meta['_seed'])
 
     # Generating the background field to be added to the photon noise
     # contaminated field.
     bgnd = rng.normal(
         loc=mean_bgnd, scale=sigma,
-        size=ph_noise_field_file.field.shape,
+        size=sim_meta['ext_shape'],
     )
     bgnd = np.where(
         bgnd < 0, 0, bgnd
@@ -63,11 +85,10 @@ def background(sources_file,
 
     return Field(
         bgnd,
-        seed=ph_noise_field_file.metadata['_seed'],
+        seed=sim_meta['_seed'],
         sources_file=str(sources_file),
         ph_noise_file=ph_noise_field_filename,
         bkgnd_file=None,
-        psf_file=None,
         gain_map_file=None,
         dk_c_file=None
     )

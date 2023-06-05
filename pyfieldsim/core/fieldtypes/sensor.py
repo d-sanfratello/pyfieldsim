a2cd50c4f89a32bd7c5a388a3bb9b84a9def228c
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-06-05 11:03:17 +0200

    implemented save_metadata and read_metadata. Now reads metadata from sources file.

--- pyfieldsim/core/fieldtypes/sensor.py
+++ pyfieldsim/core/fieldtypes/sensor.py
@@ -3,25 +3,36 @@ import numpy as np
 from pathlib import Path
 
 from pyfieldsim.core.fieldtypes.field import Field
+from pyfieldsim.utils.metadata import read_metadata, save_metadata
 
 
 def create_gain_map(sources_file, mean_gain, rel_var):
     sources_file = Path(sources_file)
-    sources_file = Field.from_sources(sources_file)
+    sim_meta = read_metadata(sources_file)
 
-    rng = np.random.default_rng(seed=sources_file.metadata['_seed'])
+    rng = np.random.default_rng(seed=sim_meta['_seed'])
 
     # Generating the gain map field to be multiply the read data.
     gain_map = rng.normal(
         loc=mean_gain, scale=rel_var * mean_gain,
-        size=sources_file.field.shape
+        size=sim_meta['ext_shape']
+    )
+
+    metadata = {
+        "mean": mean_gain,
+        "rel_var": rel_var
+    }
+
+    save_metadata(
+        metadata=metadata,
+        filename='G' + sources_file.name[1:]
     )
 
     gain_map = np.where(gain_map < 0, 0, gain_map)
 
     return Field(
         gain_map,
-        seed=sources_file.metadata['_seed'],
+        seed=sim_meta['_seed'],
         sources_file=str(sources_file),
         ph_noise_file='P' + str(sources_file)[1:],
         bkgnd_file='B' + str(sources_file)[1:],
@@ -31,16 +42,16 @@ def create_gain_map(sources_file, mean_gain, rel_var):
     )
 
 
-def create_dark_current(
-        sources_file,
-        b_fraction=0.1,
-        rel_var=0.01
-):
+def create_dark_current(sources_file,
+                        b_fraction=0.1,
+                        rel_var=0.01):
     sources_file = Path(sources_file)
     background_filename = 'B' + sources_file.name[1:]
     background_file = Field.from_field(background_filename)
 
-    rng = np.random.default_rng(seed=background_file.metadata['_seed'])
+    sim_meta = read_metadata(sources_file)
+
+    rng = np.random.default_rng(seed=sim_meta['_seed'])
 
     b_mean = np.mean(background_file.field)
     dk_c_mean = b_mean * b_fraction
@@ -48,14 +59,24 @@ def create_dark_current(
     # Generating the dark current field for the simulated CCD.
     dk_c_field = rng.normal(
         loc=dk_c_mean, scale=rel_var * dk_c_mean,
-        size=background_file.field.shape
+        size=sim_meta['ext_shape']
+    )
+
+    metadata = {
+        "mean": dk_c_mean,
+        "rel_var": rel_var
+    }
+
+    save_metadata(
+        metadata=metadata,
+        filename='C' + sources_file.name[1:]
     )
 
     dk_c_field = np.where(dk_c_field < 0, 0, dk_c_field)
 
     return Field(
         dk_c_field,
-        seed=background_file.metadata['_seed'],
+        seed=sim_meta['_seed'],
         sources_file=str(sources_file),
         ph_noise_file='P' + str(sources_file)[1:],
         bkgnd_file='B' + str(sources_file)[1:],

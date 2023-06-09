f24f7f199da8772b6d2c4be3e2e786e12b819736
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-06-09 18:18:44 +0200

    Now it takes several arguments and is able to generate different type of noise-contaminated fields.

--- pyfieldsim/pipelines/simulate_observation.py
+++ pyfieldsim/pipelines/simulate_observation.py
@@ -1,34 +1,5 @@
-#         # Simulation advancement at each step after initialization.
-#         self.add_photon_noise(
-#             delta_time=delta_time,
-#             force=True,
-#             multiply=False
-#         )
-#         self.add_background(
-#             fluct=background_fluct,
-#             snr=snr,
-#             rel_var=bgnd_rel_var,
-#             force=True
-#         )
-#         self.apply_psf(kernel=kernel, force=True)
-#
-#         self.create_gain_map(
-#             mean_gain=gain_mean,
-#             rel_var=gain_rel_var,
-#             force=True
-#         )
-#         self.create_dark_current(
-#             b_fraction=dk_c_fraction,
-#             rel_var=dk_c_rel_var,
-#             dk_c=dk_c,
-#             force=True
-#         )
-#
-#         self.recorded_field = self.gain_map \
-#           * self.w_psf_field \
-#           + self.dark_current
-
 import argparse as ag
+import numpy as np
 import os
 import scipy.signal as scipysig
 
@@ -36,6 +7,7 @@ from pathlib import Path
 
 from pyfieldsim.core.fieldtypes.field import Field
 from pyfieldsim.core.psf import GaussKernel
+from pyfieldsim.utils.metadata import read_metadata
 
 
 def main():
@@ -44,9 +16,21 @@ def main():
         description='',
     )
     parser.add_argument('sources')
+    parser.add_argument('-p', action='store_true',
+                        dest='ph_contamianated', default=False,
+                        help="")
+    parser.add_argument('-b', action='store_true',
+                        dest='background', default=False,
+                        help="")
     parser.add_argument('-k', '--psf-width', type=float,
-                        dest='psf_width', default=3,
+                        dest='psf_width', default=None,
                         help='')
+    parser.add_argument('-g', action='store_true',
+                        dest='gain_map', default=False,
+                        help="")
+    parser.add_argument('-c', action='store_true',
+                        dest='dark_current', default=False,
+                        help="")
     parser.add_argument("-o", "--output",
                         dest='out_folder', default=None,
                         help="")
@@ -59,75 +43,67 @@ def main():
         out_folder = Path(args.out_folder)
 
     sources_file = Path(args.sources)
-    ph_noise_contaminated_filename = 'P' + sources_file.name[1:]
-    background_filename = 'B' + sources_file.name[1:]
-    gain_map_filename = 'G' + sources_file.name[1:]
-    dark_current_filename = 'C' + sources_file.name[1:]
+    metadata = read_metadata(sources_file)
 
-    observation_filename = 'O' + sources_file.name[1:]
-    observation_filename = out_folder.joinpath(observation_filename)
+    if args.ph_contaminated:
+        ph_contaminated_filename = 'P' + sources_file.name[1:]
+        ph_noise_contaminated = Field.from_field(ph_contaminated_filename)
 
-    ph_noise_contaminated = Field.from_field(ph_noise_contaminated_filename)
-    background = Field.from_field(background_filename)
-    gain_map = Field.from_field(gain_map_filename)
-    dark_current = Field.from_field(dark_current_filename)
+        field = ph_noise_contaminated.field
+    else:
+        field = Field.from_sources(sources_file).field
+        ph_contaminated_filename = None
 
-    psf = GaussKernel(sigma=args.psf_width)
+    if args.background:
+        background_filename = 'B' + sources_file.name[1:]
+        background = Field.from_field(background_filename)
 
-    noise_contaminated = ph_noise_contaminated + background
-    field_w_psf = scipysig.convolve2d(
-        noise_contaminated,
-        psf.kernel,
-        mode='same'
-    )
-    field_w_psf = Field(
-        field=field_w_psf,
-        seed=ph_noise_contaminated.metadata['_seed'],
+        field = field + background
+    else:
+        background_filename = None
+
+    if args.psf_width is not None:
+        psf = GaussKernel(sigma=args.psf_width)
+        field = scipysig.convolve2d(
+            field,
+            psf.kernel,
+            mode='same'
+        )
+
+        field = np.where(field < 0, 0, field)
+        field = np.round(field, dtype=int)
+
+    if args.gain_map:
+        gain_map_filename = 'G' + sources_file.name[1:]
+        gain_map = Field.from_field(gain_map_filename)
+
+        np.round(gain_map * field, dtype=int)
+    else:
+        gain_map_filename = None
+
+    if args.dark_current:
+        dark_current_filename = 'C' + sources_file.name[1:]
+        dark_current = Field.from_field(dark_current_filename)
+
+        field = field + dark_current
+    else:
+        dark_current_filename = None
+
+    field = Field(
+        field,
+        seed=metadata['seed'],
         sources_file=sources_file,
-        ph_noise_file=ph_noise_contaminated_filename,
-        bkgnd_file=background_filename,
-    )
-    field_w_gain_map = Field(
-        gain_map * field_w_psf,
-        seed=ph_noise_contaminated.metadata['_seed'],
-        sources_file=sources_file,
-        ph_noise_file=ph_noise_contaminated_filename,
-        bkgnd_file=background_filename,
-        gain_map_file=gain_map_filename,
-    )
-    field_w_dk_current = Field(
-        field_w_gain_map + dark_current,
-        seed=ph_noise_contaminated.metadata['_seed'],
-        sources_file=sources_file,
-        ph_noise_file=ph_noise_contaminated_filename,
+        ph_noise_file=ph_contaminated_filename,
         bkgnd_file=background_filename,
         gain_map_file=gain_map_filename,
         dk_c_file=dark_current_filename
     )
 
-    field_w_dk_current.export_field(filename=observation_filename)
+    observation_filename = 'O' + sources_file.name[1:]
+    observation_filename = out_folder.joinpath(observation_filename)
+
+    field.export_field(filename=observation_filename)
 
 
 if __name__ == "__main__":
     main()
-
-
-# if __name__ == "__main__":
-#     # -------------------------------------------------------------------------
-#     # Complete operation
-#     # -------------------------------------------------------------------------
-#
-#     # counting single stars
-#     stars, coords = observation.count_single_stars()
-#     print(f'{len(stars)} single stars')
-#
-#     psf = GaussKernel(sigma=3)
-#
-#     field.record_field(kernel=psf,
-#                        delta_time=1000, snr=10, bgnd_rel_var=0.05,
-#                        gain_mean=1, gain_rel_var=0.01,
-#                        dk_c_fraction=0.1, dk_c_rel_var=0.01, dk_c=1,
-#                        force=True)
-#
-#     field.save_field(name='stars')
-#     field.show_field('exposure')

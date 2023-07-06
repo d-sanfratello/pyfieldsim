9f3c7f7f4a5fbd6e3f5dd7f0f9e854d02c0050e0
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-07-06 17:22:39 +0200

    Improved precision in amplitude of saved star

--- pyfieldsim/utils/save_stars.py
+++ pyfieldsim/utils/save_stars.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from pathlib import Path
 
-from metadata import save_metadata
+from .metadata import save_metadata
 
 
 def save_stars(stars, data_file, options=None):
@@ -21,7 +21,7 @@ def save_stars(stars, data_file, options=None):
     for _, s in enumerate(stars):
         mu_x[_] = np.round(s.mu[0], decimals=1)
         mu_y[_] = np.round(s.mu[1], decimals=1)
-        A[_] = np.round(s.A, decimals=1)
+        A[_] = np.round(s.A, decimals=3)
 
     metadata = {
         'n_stars': n_stars,

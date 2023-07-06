bccd6f0b8dfd003b446ecab4baa3d7da2f55a7c3
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-07-06 17:13:19 +0200

    Updated with latest save_stars implementation update.

--- pyfieldsim/pipelines/find_stars.py
+++ pyfieldsim/pipelines/find_stars.py
@@ -15,7 +15,8 @@ from scipy.stats import poisson, norm
 
 from pyfieldsim.core.fieldtypes.field import Field
 from pyfieldsim.core.stars import new_star
-from pyfieldsim.utils.metadata import read_metadata, save_metadata
+from pyfieldsim.utils.metadata import read_metadata
+from pyfieldsim.utils.save_stars import save_stars
 
 
 def main():
@@ -668,34 +669,6 @@ def dist(x1, x2):
 
     d = np.sqrt(((x2 - x1) ** 2).sum())
     return d
-
-
-def save_stars(stars, data_file, options=None):
-    stars = np.asarray(stars)
-    n_stars = stars.shape[0]
-
-    output = Path('R' + data_file.stem[1:]).with_suffix('.h5')
-    suffix = output.suffix
-    if options is not None:
-        output = Path(output.stem + f"_{''.join(options)}").with_suffix(suffix)
-
-    mu_x = np.zeros(n_stars, dtype=float)
-    mu_y = np.zeros(n_stars, dtype=float)
-    A = np.zeros(n_stars, dtype=float)
-
-    for _, s in enumerate(stars):
-        mu_x[_] = np.round(s.mu[0], decimals=1)
-        mu_y[_] = np.round(s.mu[1], decimals=1)
-        A[_] = np.round(s.A, decimals=1)
-
-    metadata = {
-        'n_stars': n_stars,
-        'mu_x': mu_x.tolist(),
-        'mu_y': mu_y.tolist(),
-        'A': A.tolist()
-    }
-
-    save_metadata(metadata, output)
 
 
 if __name__ == "__main__":

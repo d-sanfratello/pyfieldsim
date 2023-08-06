975c4d0e7f2a1f7294e9e79bdfa1f3452a51efa3
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-08-06 08:59:03 +0200

    Star object now saves formats to print medians to the right significant digits.

--- pyfieldsim/core/stars/star.py
+++ pyfieldsim/core/stars/star.py
@@ -6,7 +6,7 @@ from .point_star import PointStar
 
 
 class Star(PointStar):
-    def __init__(self, A, mu, sigma):
+    def __init__(self, A, mu, sigma, fmts):
         super(Star, self).__init__(A, mu)
 
         self.sigma = sigma
@@ -15,9 +15,23 @@ class Star(PointStar):
             mean=[self.mu[0], self.mu[1]],
             cov=self.sigma * np.eye(2))
 
+        self.__fmts = fmts
+
     def __call__(self, x):
         return self.A * mvn.pdf(x)
 
+    @property
+    def fmt_A(self):
+        return self.__fmts[0]
 
-def new_star(A, mu, sigma):
-    return Star(A, mu, sigma)
+    @property
+    def fmt_mu_x(self):
+        return self.__fmts[1]
+
+    @property
+    def fmt_mu_y(self):
+        return self.__fmts[2]
+
+
+def new_star(A, mu, sigma, fmts):
+    return Star(A, mu, sigma, fmts)

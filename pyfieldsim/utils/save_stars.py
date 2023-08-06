144fdade9cbc2406dc55a8a8ba238925f0a3586a
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-08-06 09:02:00 +0200

    save_stars now correctly handles significant digits and saves the ids for each star.

--- pyfieldsim/utils/save_stars.py
+++ pyfieldsim/utils/save_stars.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from .metadata import save_metadata
 
 
-def save_stars(stars, data_file, options=None):
+def save_stars(stars, data_file, saved_ids, options=None):
     stars = np.asarray(stars)
     n_stars = stars.shape[0]
 
@@ -19,15 +19,33 @@ def save_stars(stars, data_file, options=None):
     A = np.zeros(n_stars, dtype=float)
 
     for _, s in enumerate(stars):
-        mu_x[_] = np.round(s.mu[0], decimals=1)
-        mu_y[_] = np.round(s.mu[1], decimals=1)
-        A[_] = np.round(s.A, decimals=3)
+        mu_x[_] = np.round(
+            s.mu[0],
+            decimals=find_rounding_decimals(s.fmt_mu_x(s.mu[0]))
+        )
+        mu_y[_] = np.round(
+            s.mu[1],
+            decimals=find_rounding_decimals(s.fmt_mu_y(s.mu[1]))
+        )
+        A[_] = np.round(
+            s.A,
+            decimals=find_rounding_decimals(s.fmt_A(s.A))
+        )
 
     metadata = {
         'n_stars': n_stars,
         'mu_x': mu_x.tolist(),
         'mu_y': mu_y.tolist(),
-        'A': A.tolist()
+        'A': A.tolist(),
+        'id': saved_ids
     }
 
     save_metadata(metadata, output)
+
+
+def find_rounding_decimals(str_):
+    mantissa = str_.split('e')[0]
+    exponent = str_.split('e')[1]
+    decimals = len(mantissa.split('.')[1]) - int(exponent) - 1
+
+    return decimals

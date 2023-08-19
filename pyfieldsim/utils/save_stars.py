ca53ccdecc0ec5e88dbed086ebfd59dd222fdf25
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-08-20 00:56:03 +0200

    Updated arguments of save_stras with psf hypothesis. Added errors on A to exported quantities. Updated saved values either with formats or with rounding errors. Updated exported metadata. Updated load_stars with changes in metadata structure. Fixed find_rounding_decimals returning wrong significant figures. Created function to round significant digits from errorbars.

--- pyfieldsim/utils/save_stars.py
+++ pyfieldsim/utils/save_stars.py
@@ -6,7 +6,7 @@ from .metadata import save_metadata, read_metadata
 from pyfieldsim.core.stars.star import new_star
 
 
-def save_stars(stars, data_file, saved_ids, options=None):
+def save_stars(stars, data_file, saved_ids, hyp_psf, options=None):
     stars = np.asarray(stars)
     n_stars = stars.shape[0]
 
@@ -19,23 +19,43 @@ def save_stars(stars, data_file, saved_ids, options=None):
     mu_y = np.zeros(n_stars, dtype=float)
     A = np.zeros(n_stars, dtype=float)
     pos_errors = np.zeros((n_stars, 2, 2), dtype=float)
+    A_errors = np.zeros((n_stars, 2), dtype=float)
 
     sigma = stars[0].sigma[0, 0]
 
     for _, s in enumerate(stars):
-        mu_x[_] = np.round(
-            s.mu[0],
-            decimals=find_rounding_decimals(s.fmt_mu_x(s.mu[0]))
-        )
-        mu_y[_] = np.round(
-            s.mu[1],
-            decimals=find_rounding_decimals(s.fmt_mu_y(s.mu[1]))
-        )
-        A[_] = np.round(
-            s.A,
-            decimals=find_rounding_decimals(s.fmt_A(s.A))
-        )
+        try:
+            decimals = find_rounding_decimals(s.fmt_mu_x(s.mu[0]))
+            value = np.round(s.mu[0], decimals=decimals)
+        except TypeError:
+            value_str = find_rounding_from_errs(s.mu[0], s.pos_error[0])
+            value = float(value_str)
+        finally:
+            # noinspection PyUnboundLocalVariable
+            mu_x[_] = value
+
+        try:
+            decimals = find_rounding_decimals(s.fmt_mu_y(s.mu[1]))
+            value = np.round(s.mu[1], decimals=decimals)
+        except TypeError:
+            value_str = find_rounding_from_errs(s.mu[1], s.pos_error[1])
+            value = float(value_str)
+        finally:
+            # noinspection PyUnboundLocalVariable
+            mu_y[_] = value
+
+        try:
+            decimals = find_rounding_decimals(s.fmt_A(s.A))
+            value = np.round(s.A, decimals=decimals)
+        except TypeError:
+            value_str = find_rounding_from_errs(s.A, s.A_error)
+            value = float(value_str)
+        finally:
+            # noinspection PyUnboundLocalVariable
+            A[_] = value
+
         pos_errors[_] = np.asarray(s.pos_error)
+        A_errors[_] = np.asarray(s.A_error)
 
     metadata = {
         'n_stars': n_stars,
@@ -44,7 +64,9 @@ def save_stars(stars, data_file, saved_ids, options=None):
         'A': A.tolist(),
         'sigma': (sigma * np.ones(n_stars)).tolist(),
         'id': saved_ids,
-        'pos_errors': pos_errors.tolist()
+        'pos_errors': pos_errors.tolist(),
+        'A_errors': A_errors.tolist(),
+        'hyp_psf': hyp_psf
     }
 
     save_metadata(metadata, output)
@@ -63,7 +85,8 @@ def load_stars(data_file, options=None):
         metadata['mu_x'],
         metadata['mu_y'],
         metadata['sigma'],
-        metadata['pos_errors']
+        metadata['pos_errors'],
+        metadata['A_errors']
     )
 
     stars = [
@@ -71,12 +94,13 @@ def load_stars(data_file, options=None):
             A=float(A),
             mu=(float(mu_x), float(mu_y)),
             sigma=float(sigma) * np.eye(2),
-            pos_error=np.asarray(p_err).flatten()
+            pos_error=np.asarray(p_err).flatten(),
+            A_error=np.asarray(A_err)
         )
-        for A, mu_x, mu_y, sigma, p_err in star_obj
+        for A, mu_x, mu_y, sigma, p_err, A_err in star_obj
     ]
 
-    return stars, metadata['pos_errors'], metadata['id']
+    return stars, metadata['pos_errors'], metadata['id'], metadata['hyp_psf']
 
 
 def find_rounding_decimals(str_):
@@ -84,8 +108,19 @@ def find_rounding_decimals(str_):
     exponent = str_.split('e')[1]
 
     if mantissa.find('.') > -1:
-        decimals = len(mantissa.split('.')[1]) - int(exponent) - 1
+        decimals = len(mantissa.split('.')[1]) - int(exponent)
     else:
-        decimals = 0 - int(exponent) - 1
+        decimals = 0 - int(exponent)
 
     return decimals
+
+
+def find_rounding_from_errs(value, errors):
+    min_error = min(errors[0], errors[1])
+    odg_err = np.floor(np.log10(min_error)).astype(int)
+    odg_meas = np.floor(np.log10(value)).astype(int)
+
+    odg = odg_meas - odg_err
+    fmt = f"{{0:{odg}}}".format
+
+    return fmt(value)

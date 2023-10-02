e3ff3257fc90c176b0f0167c608dd2d7d8e57e13
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-10-02 12:38:20 +0200

    Added documentation on sstars

--- pyfieldsim/core/stars/point_star.py
+++ pyfieldsim/core/stars/point_star.py
@@ -2,6 +2,10 @@ import numpy as np
 
 
 class PointStar:
+    """
+    Class to define a delta-like star, containing the brightness and the
+    centroid position.
+    """
     def __init__(self, A, mu):
         self.A = A
         self.mu = mu
@@ -14,4 +18,20 @@ class PointStar:
 
 
 def new_point_star(A, mu):
+    """
+    Function that generates a new `PointStar` for given brightness and
+    position.
+
+    Parameters
+    ----------
+    A: `number`
+        The brightness of the star
+    mu: `numpy.ndarray` of shape (2,)
+        The centroid position of the star.
+
+    Returns
+    -------
+    PointStar
+        An instance of a new `PointStar`.
+    """
     return PointStar(A, mu)
--- pyfieldsim/core/stars/star.py
+++ pyfieldsim/core/stars/star.py
@@ -6,6 +6,12 @@ from .point_star import PointStar
 
 
 class Star(PointStar):
+    """
+    Class to define a PSF-convolved star, containing the brightness and the
+    centroid position, the width of the PSF, errors on retireved position
+    and brightness and the formats to write the errorbars to the correct
+    number of significant digits.
+    """
     def __init__(self, A, mu, sigma, fmts=None, pos_error=None, A_error=None):
         super(Star, self).__init__(A, mu)
 
@@ -47,4 +53,30 @@ class Star(PointStar):
 
 
 def new_star(A, mu, sigma, fmts=None, pos_error=None, A_error=None):
+    """
+    Function that generates a new `Star` for given brightness, position,
+    PSF width, formats and errors.
+
+    Parameters
+    ----------
+    A: `number`
+        The brightness of the star
+    mu: `numpy.ndarray` of shape (2,)
+        The centroid position of the star.
+    sigma: `number`
+        The width of the gaussian PSF.
+    fmts: `tuple` of `format()` methods
+        A tuple of three format methods for the brightness and x and y errors.
+    pos_error: `iterable`
+        An iterable of the errorbars for the centroid position, such as [
+        low_x_err, high_x_err, low_y_err, high_y_err].
+    A_error: `iterable`
+        An iterable with the errorbars on retrieved brightness, in the form
+        [low_A_err, high_A_err].
+
+    Returns
+    -------
+    Star
+        An instance of a new `Star`.
+    """
     return Star(A, mu, sigma, fmts=fmts, pos_error=pos_error, A_error=A_error)
